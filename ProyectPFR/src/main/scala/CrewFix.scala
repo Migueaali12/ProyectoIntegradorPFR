00ca34677849b344d6afd8f06f8382a6ccde2e96
Migueaali12 <malvarez11@utpl.edu.ec>
2023-02-01 21:16:24 -0500

    Proyect0 1.1

--- ProyectPFR/src/main/scala/CrewFix.scala
+++ ProyectPFR/src/main/scala/CrewFix.scala
@@ -1,3 +1,58 @@
+import com.github.tototoshi.csv.CSVReader
+import play.api.libs.json._
+import java.io.File
+import scala.util.{Failure, Success, Try}
+import scala.util.matching.Regex
+
 object CrewFix extends App {
+
+  val reader = CSVReader.open(new File("src/movie_dataset.csv"))
+  val data = reader.allWithHeaders()
+  reader.close()
+
+  def replacePattern(original: String): String = {
+    var txtOr = original
+    val pattern: Regex = "(\\s\"(.*?)\",)".r
+    for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      val replacementText = m.replace("'", "-u0027")
+      txtOr = txtOr.replace(textOriginal, replacementText)
+    }
+    txtOr
+  }
+
+  def replacePattern2(original: String): String = {
+    var txtOr = original
+    val pattern: Regex = "([a-z]\\s\"(.*?)\"\\s*[A-Z])".r
+    for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      val replacementText = m.replace("\"", "-u0022")
+      txtOr = txtOr.replace(textOriginal, replacementText)
+    }
+    txtOr
+  }
+
+  def replacePattern3(original: String): String = {
+    var txtOr = original
+    val pattern: Regex = "(:\\s'\"(.*?)',)".r
+    for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      val replacementText = m.replace("\"", "-u0022")
+      txtOr = txtOr.replace(textOriginal, replacementText)
+    }
+    txtOr
+  }
+
+  val crew = data
+    .map(row => row("crew"))
+    .map(replacePattern2)
+    .map(replacePattern)
+    .map(replacePattern3)
+    .map(text => text.replace("'", "\""))
+    .map(text => text.replace("-u0027", "'"))
+    .map(text => text.replace("-u0022", "\\\""))
+    .map(text => Try(Json.parse(text)))
+    .filter(_.isSuccess)
+    .size
 
 }
